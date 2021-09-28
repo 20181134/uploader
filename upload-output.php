d54177dcebd6ac62aea3d252b610a72e30dee066
20181134 <91303066+20181134@users.noreply.github.com>
2021-09-28 23:06:56 +0900

    Update upload-output.php

--- upload-output.php
+++ upload-output.php
@@ -5,11 +5,15 @@
 </head>
 <body>
   <?php
+  // $_FILES関数でアップロードされたファイルの名前を取得、アップロードされたファイルかどうかを確認
     if (is_uploaded_file($_FILES['file1']['tmp_name'])) {
+      // upload ファイルの有無を確認
       if (!file_exists('upload')) {
         mkdir('upload');
       }
+      // $file関数に"upload/ファイル名"を代入
       $file='upload/'.basename($_FILES['file1']['name']);
+      // 一時ファイルを$fileに保存
       if (move_uploaded_file($_FILES['file1']['tmp_name'], $file)) {
         echo $file, 'のアップロードが完了しました';
         echo '<p><img src="', $file, '"</p>';
